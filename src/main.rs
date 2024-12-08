518cfa049f145ec45a653383e52939e21182da58
Chris Patuzzo <chris@patuzzo.co.uk>
2024-12-08 19:27:55 +0000

    Implement the main function

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,29 @@ mod pangram_machine;
 
 use initial_text_constants::*;
 use number_profiles::*;
-use pairwise_sum::*;
 use pangram_machine::*;
+use std::io::{stdin, stdout, Write};
+use std::process::exit;
 
 fn main() {
-    println!("Hello, world!");
+    print!("Enter intial text: ");
+    let mut initial_text = String::new();
+    stdout().flush().unwrap();
+    stdin().read_line(&mut initial_text).unwrap();
+
+    let profiles = number_profiles();
+    let constants = initial_text_constants(&initial_text, &profiles);
+    println!("Initial constants: {:?}", constants);
+    println!("\nRunning Pangram Machine Mark II...");
+
+    let mut num_solutions = 0;
+    pangram_machine(&constants, &profiles, |solution| {
+        println!("EUREKA! {:?}", solution);
+        num_solutions += 1;
+    });
+
+    if num_solutions == 0 {
+        println!("Unfortunately, no solutions were found.");
+        exit(1);
+    }
 }
