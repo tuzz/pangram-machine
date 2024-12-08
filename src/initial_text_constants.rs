793910fa7de3c841ef3ca3d36604b24ae8b707b7
Chris Patuzzo <chris@patuzzo.co.uk>
2024-12-08 19:02:12 +0000

    Fix mistakenly adding plurals to the initial_text_constants

--- src/initial_text_constants.rs
+++ src/initial_text_constants.rs
@@ -17,14 +17,6 @@ pub fn initial_text_constants(string: &str, profiles: &[[usize; 26]; 51]) -> [us
         profile = pairwise_sum(&profile, &profiles[count]); // Add letters from already-known number words.
     }
 
-    for letter in LETTERS_NOT_IN_NUMBER_WORDS.iter() {
-        let is_plural = profile[(*letter as u8 - b'a') as usize] != 1;
-
-        if is_plural {
-            profile[18] += 1; // Add a plural 's' is the already-known number is not 'one'.
-        }
-    }
-
     profile
 }
 
@@ -38,14 +30,13 @@ mod test {
         let profiles = number_profiles();
         let constants = initial_text_constants("This pangram contains and", &profiles);
 
-                            // a  b  c  d  e  f  g  h  i  j  k  l  m  n   o   p  q  r  s   t  u  v  w  x  y  z
-        assert_eq!(constants, [5, 1, 2, 2, 7, 2, 2, 2, 4, 1, 1, 1, 2, 10, 11, 2, 1, 2, 13, 7, 1, 2, 5, 1, 1, 1]);
+                            // a  b  c  d  e  f  g  h  i  j  k  l  m  n   o   p  q  r  s  t  u  v  w  x  y  z
+        assert_eq!(constants, [5, 1, 2, 2, 7, 2, 2, 2, 4, 1, 1, 1, 2, 10, 11, 2, 1, 2, 8, 7, 1, 2, 5, 1, 1, 1]);
 
         // Note that this example is from page 5 of the paper:
         // https://www.leesallows.com/files/In%20Quest%20of%20a%20Pangram1.pdf#page=5
         //
-        // However, it excludes the plurals for letter in the number words.
-        // These can't be pre-determined because some of the ranges start from
-        // 1 and could mean no plural is added. The algorithm adds them later.
+        // However, it excludes plurals since they are added by the algorithm.
+        // These can't be pre-determined now because some ranges start from 1.
     }
 }
